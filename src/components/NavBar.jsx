096f6945f6756957475067582f917192e3f55abc
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-02-08 19:35:08 -0300

    add animats

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ import { AiOutlineMenu, AiOutlineClose, AiFillHome } from 'react-icons/ai'
 import { useDispatch, useSelector } from 'react-redux'
 import { searchProductAsync } from '@/redux/slices/products/thunk'
 import { useNavigate } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const NavBar = () => {
   const dispatch = useDispatch()
@@ -56,51 +56,119 @@ const NavBar = () => {
     visible: { opacity: 1, x: 0 },
     hidden: { opacity: 0, x: -100 },
   }
+  const list2 = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.5,
+        staggerDirection: 1,
+        delayChildren: 1,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: 'afterChildren',
+      },
+    },
+  }
+
+  const item2 = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  }
 
   return (
     <>
-      <div
-        className='sticky top-0  right-0 z-50 flex
+      <div className='flex w-full flex-col justify-center  gap-5 bg-gray-900 lg:hidden'>
+        <motion.div
+          initial='hidden'
+          animate='visible'
+          variants={list}
+          className='sticky top-0  right-0 z-50 flex
         items-center justify-between
       overflow-hidden bg-gray-900 text-white transition-all duration-500 ease-linear'
-      >
-        <div
-          className={`flex h-auto w-full items-start bg-gray-900 px-5 py-3  transition-all duration-500 ease-linear lg:hidden `}
         >
-          {!open ? (
-            <button className=' text-white' onClick={() => setOpen(!open)}>
-              <AiOutlineMenu size={30} />
-            </button>
-          ) : (
-            <button className=' text-white' onClick={() => setOpen(!open)}>
-              <AiOutlineClose size={30} />
-            </button>
-          )}
-        </div>
+          <motion.div
+            variants={item}
+            className={`flex h-auto w-full items-start bg-gray-900 px-5 py-3  transition-all duration-500 ease-linear lg:hidden `}
+          >
+            {!open ? (
+              <button className=' text-white' onClick={() => setOpen(!open)}>
+                <AiOutlineMenu size={30} />
+              </button>
+            ) : (
+              <button className=' text-white' onClick={() => setOpen(!open)}>
+                <AiOutlineClose size={30} />
+              </button>
+            )}
+          </motion.div>
 
-        <div className='flex items-center justify-center gap-5 px-5 lg:hidden '>
-          <Link to='/' className=''>
-            <AiFillHome size={30} />
-          </Link>
-          <Link to='/products'>
-            <FaStoreAlt size={30} />
-          </Link>
-          <Link to='/cart'>
-            <AiOutlineShoppingCart size={30} />
-          </Link>
-        </div>
+          <motion.div variants={item}>
+            <motion.ul
+              initial='hidden'
+              animate='visible'
+              variants={list2}
+              className='flex items-center justify-center gap-5 px-5 lg:hidden '
+            >
+              <motion.li variants={item2}>
+                <Link to='/' className=''>
+                  <AiFillHome size={30} />
+                </Link>
+              </motion.li>
+              <motion.li variants={item2}>
+                <Link to='/products'>
+                  <FaStoreAlt size={30} />
+                </Link>
+              </motion.li>
+              <motion.li variants={item2}>
+                <Link to='/cart'>
+                  <AiOutlineShoppingCart size={30} />
+                </Link>
+              </motion.li>
+            </motion.ul>
+          </motion.div>
+        </motion.div>
+
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ x: -300 }}
+              transition={{ duration: 1 }}
+              animate={{ x: 0 }}
+              exit={{ x: -300 }}
+              className=''
+            >
+              <motion.form
+                variants={item}
+                onSubmit={handleSubmit}
+                className='flex w-full items-center justify-center gap-5 overflow-hidden bg-gray-900 py-3 lg:mx-auto'
+              >
+                <input
+                  type='search'
+                  onChange={({ target }) => handleSearch(target)}
+                  className='w-auto rounded-lg border border-gray-400 px-5 py-1 text-gray-900 outline-none transition-all duration-500 lg:w-[50%] '
+                  placeholder='Buscar..'
+                  name=''
+                  id=''
+                />
+                <AiOutlineSearch className='cursor-pointer text-2xl text-white transition-all duration-500 ease-linear hover:origin-top hover:rotate-12 hover:text-gray-500' />
+              </motion.form>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
+
       <motion.header
         initial='hidden'
         animate='visible'
         variants={list}
         id='navbar'
-        className={` ${
-          !open ? 'h-0 -translate-x-[100%] bg-gray-900' : 'h-auto translate-x-[0%] bg-gray-900 py-1'
-        } sticky top-0
+        className={`sticky top-0
         z-50
       mx-auto
-      h-full w-full max-w-screen-xl    flex-col    gap-5 overflow-hidden bg-gray-900 px-5  shadow-lg transition-all duration-700  ease-linear lg:flex  lg:h-max lg:translate-x-0 lg:flex-row lg:items-center lg:justify-between lg:py-1`}
+       w-full max-w-screen-xl    flex-col    gap-5 overflow-hidden bg-gray-900 px-5  shadow-lg  lg:flex  lg:h-max lg:translate-x-0 lg:flex-row lg:items-center lg:justify-between lg:py-1`}
       >
         <motion.div variants={item}>
           <Link to='/' className='hidden rounded-3xl lg:flex'>
@@ -116,7 +184,7 @@ const NavBar = () => {
         <motion.form
           variants={item}
           onSubmit={handleSubmit}
-          className='flex w-full items-center justify-center gap-5 lg:mx-auto'
+          className='hidden w-full items-center justify-center gap-5 lg:mx-auto lg:flex'
         >
           <input
             type='search'
@@ -130,7 +198,7 @@ const NavBar = () => {
         </motion.form>
 
         <motion.nav variants={item} className=''>
-          <ul className='flex flex-col items-center justify-between gap-12   pt-5 lg:flex-row lg:items-center lg:justify-center lg:gap-5 lg:p-3'>
+          <ul className='flex flex-col items-center justify-between gap-12    lg:flex-row lg:items-center lg:justify-center lg:gap-5 lg:p-3'>
             <li
               onClick={upScroll}
               className='hidden items-center  gap-3 font-sans text-lg font-semibold text-white transition-all duration-500 hover:text-gray-500 lg:flex'
