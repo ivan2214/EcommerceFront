2d23205e828b555a96470910d38904a8af02fb2c
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-02-08 14:38:57 -0300

    add scroll to top

--- src/components/CardCategory.jsx
+++ src/components/CardCategory.jsx
@@ -4,11 +4,15 @@ import Svg from '@/assets/Svg'
 import { Link, useParams } from 'react-router-dom'
 
 const CardCategory = ({ name = 'category', logo, id }) => {
+  const upScroll = () => {
+    window.scroll(0)
+  }
+
   return (
-    <Link to={`/products/category/${name}`}>
-      <article className='flex  min-w-[100px] max-w-[150px] p-5 flex-col items-center gap-4 rounded-lg shadow-md lg:w-56  lg:max-w-xs lg:p-6'>
+    <Link to={`/products/category/${name}`} onClick={upScroll}>
+      <article className='flex  min-w-[100px] max-w-[150px] flex-col items-center gap-4 rounded-lg p-5 shadow-md lg:w-56  lg:max-w-xs lg:p-6'>
         <Svg className='text-blue-900' />
-        <h3 className='capitalize text-center text-gray-900'>{name}</h3>
+        <h3 className='text-center capitalize text-gray-900'>{name}</h3>
       </article>
     </Link>
   )
--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -3,12 +3,14 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const CardHome = ({ name, logo }) => {
-  
+  const upScroll = () => {
+    window.scroll(0)
+  }
 
   return (
-    <article className='flex  min-w-[150px] max-w-[150px] p-5 flex-col items-center gap-4 rounded-lg shadow-md lg:w-56  lg:max-w-xs lg:p-6'>
+    <article className='flex  min-w-[150px] max-w-[150px] flex-col items-center gap-4 rounded-lg p-5 shadow-md lg:w-56  lg:max-w-xs lg:p-6'>
       <div className='flex  flex-col  items-center  justify-between '>
-        <Link to={`/products/${name}`} className=' w-full'>
+        <Link to={`/products/${name}`} onClick={upScroll} className=' w-full'>
           <img
             className='h-[100px] max-h-[100px]   w-full rounded-lg object-contain'
             src={logo}
