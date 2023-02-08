5b3c862a26e95f4b4fa719271b3802f3e0a540bb
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-02-08 14:34:53 -0300

    add colors and more procducts backend

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const CartDetails = ({ id, title, description, price, quantity, images, categories }) => {
   return (
-    <article className='flex h-full w-full  justify-center gap-10 bg-white  p-24'>
-      <div className='flex h-full flex-col  items-start justify-between gap-5'>
+    <article className='mx-auto flex h-full  w-full max-w-6xl flex-col justify-center gap-10 border border-white  bg-white shadow-xl lg:flex-row  lg:p-24'>
+      <div className='flex h-full items-center justify-start gap-5  px-8 lg:flex-col lg:items-start  lg:justify-between'>
         {images?.data?.map((i, index) => {
           return (
             <div
               key={id}
-              className='flex h-24 w-24 items-center rounded-md bg-gray-500 object-cover'
+              className='flex h-24 w-24 items-center justify-center rounded-md bg-gray-400/60 p-5'
             >
               <img
-                className=' w-full rounded-md object-cover'
+                className=' mx-auto w-full  rounded-md object-contain'
                 /* src={`http://localhost:1337${i?.attributes?.url} `} */
                 src={`https://ecommercestrapi-back-production.up.railway.app${i?.attributes?.url} `}
                 alt=''
@@ -32,19 +32,17 @@ const CartDetails = ({ id, title, description, price, quantity, images, categori
       <section className=' w-full max-w-3xl'>
         <div className='flex h-full w-full  flex-col items-center justify-between gap-10 rounded-md  p-5 shadow-lg'>
           <div className='flex  h-full w-full flex-col items-start justify-between gap-5'>
-            <h3 className='text-lg font-bold capitalize text-green-600'>{title && title}</h3>
-            <p className='py-5 text-left text-base font-light'>
-              {description?.length > 150 ? description?.slice(0, 150) + '...' : description}
-            </p>
+            <h3 className='text-lg font-bold capitalize text-purple-500'>{title && title}</h3>
+            <p className='py-5 text-left text-base font-light'>{description}</p>
             <span>{formatAsARS(price)}</span>
-            <span className='rounded-full bg-green-500 px-5 text-white'>
+            <span className='rounded-full bg-purple-500 px-5 text-white'>
               {quantity > 0 && 'En stock.'}
             </span>
           </div>
 
           <button
             onClick={() => addCart(product, id)}
-            className='w-full rounded-full border border-green-500 py-2 text-center text-xl font-bold text-green-600'
+            className='w-full rounded-full border border-purple-500 py-2 text-center text-xl font-bold text-purple-500'
           >
             Agregar
           </button>
